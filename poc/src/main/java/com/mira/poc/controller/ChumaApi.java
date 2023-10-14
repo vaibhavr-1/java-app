5e15a4026b9060a311c31459fb30aa38bb07d530
vaibhavb <vaibhavb@cloudtradetech.com>
2023-10-14 13:52:27 +0530

    sds

---
+++ poc/src/main/java/com/mira/poc/controller/ChumaApi.java
@@ -0,0 +1,24 @@
+package com.mira.poc.controller;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChumaApi {
+    @GetMapping("/get")
+    public ResponseEntity<?> getApi() throws UnirestException {
+        HttpResponse<JsonNode> response = Unirest.get("http://65.1.91.91:31798/api/v1/employees")
+                .asJson();
+
+        int statusCode = response.getStatus();
+        return ResponseEntity.ok(response.getBody().toString());
+
+    }
+}
---
+++ poc/src/test/java/com/mira/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mira.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
